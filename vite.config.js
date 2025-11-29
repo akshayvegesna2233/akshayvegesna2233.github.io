f0026be04efee16a0eb4261972a5452041474a01
akshayvegesna2233 <128208646+akshayvegesna2233@users.noreply.github.com>
2025-11-28 22:35:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/akshayvegesna2233.github.io/", // <-- IMPORTANT
+  base: "/akshayvegesna2233.github.io/",
 });
